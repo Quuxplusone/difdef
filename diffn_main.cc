7142f2d73eb61b9734b252b60676402c86672c99
Arthur O'Dwyer <arthur@push.am>
2012-07-15 13:31:55 -0700

    Switch to getopt_long() for argument parsing.
    This does make it a lot easier to ensure that we're drop-in
    compatible with GNU diff, as well as avoiding a lot of repeated
    code.

--- diffn_main.cc
+++ diffn_main.cc
@@ -32,6 +32,8 @@
 #include <string>
 #include <vector>
 
+#include <getopt.h>
+
 #include "difdef.h"
 
 typedef Difdef::mask_t mask_t;
@@ -39,12 +41,13 @@ typedef Difdef::mask_t mask_t;
 static std::vector<std::string> g_MacroNames;
 static bool g_PrintUsingIfdefs = false;
 static bool g_PrintUnifiedDiff = false;
-static size_t g_LinesOfContext = 3;
+static size_t g_LinesOfContext = 0;
 
 static void do_error(const char *fmt, ...)
 {
     va_list ap;
     va_start(ap, fmt);
+    fputs("ERROR: ", stderr);
     vfprintf(stderr, fmt, ap);
     putc('\n', stderr);
     va_end(ap);
@@ -223,11 +226,11 @@ void verify_properly_nested_directives(const Difdef::Diff &diff, char *fnames[])
                 if (!diff.lines[i].in_file(v))
                     continue;
                 if ((is_elif || is_else || is_endif) && nest[v].empty()) {
-                    do_error("ERROR: file %s, line %d: %s with no preceding #if",
+                    do_error("file %s, line %d: %s with no preceding #if",
                              fnames[v], lineno[v], (is_elif ? "#elif" : is_else ? "#else" : "#endif"));
                 }
                 if ((is_elif || is_else) && nest[v].top() == 'e') {
-                    do_error("ERROR: file %s, line %d: unexpected %s following an #else",
+                    do_error("file %s, line %d: unexpected %s following an #else",
                              fnames[v], lineno[v], (is_elif ? "#elif" : "#else"));
                 }
                 if (is_if) {
@@ -244,7 +247,7 @@ void verify_properly_nested_directives(const Difdef::Diff &diff, char *fnames[])
     }
     for (int v=0; v < diff.dimension; ++v) {
         if (!nest[v].empty()) {
-            do_error("ERROR: at end of file %s: expected #endif", fnames[v]);
+            do_error("at end of file %s: expected #endif", fnames[v]);
         }
     }
 }
@@ -403,10 +406,6 @@ void do_print_unified_diff(const Difdef::Diff &diff, char *fnames[])
     const size_t hunk_size_in_b =
         leading_context + (last_diff_in_b - first_diff_in_b) + trailing_context;
 
-#if 0
-    printf("----- %zu %zu %zu  %zu %zu  %zu %zu\n",
-           last_diff_in_ab, last_diff_in_a, last_diff_in_b, leading_context, trailing_context, hunk_size_in_a, hunk_size_in_b);
-#endif
     /* Print the line numbers of the hunk. */
     printf("@@ -%d", (int)(first_diff_in_a - leading_context) + (hunk_size_in_a != 0));
     if (hunk_size_in_a != 1) printf(",%d", (int)hunk_size_in_a);
@@ -439,84 +438,103 @@ int main(int argc, char **argv)
 {
     std::vector<std::string> user_defined_macro_names;
 
-    int i;
-    for (i = 1; i < argc; ++i) {
-        if (argv[i][0] != '-') break;
-        if (!strcmp(argv[i], "-")) {
-            do_error("ERROR: reading from standard input is not supported");
+    static const struct option longopts[] = {
+        { "ifdef", required_argument, NULL, 'D' },
+        { "unified", no_argument, NULL, 'u' },
+        { "help", no_argument, NULL, 0 },
+    };
+    int c;
+    int longopt_index;
+    bool preceded_by_digit = false;
+    size_t ocontext = -1;
+    while ((c = getopt_long(argc, argv, "0123456789D:uU:", longopts, &longopt_index)) != -1) {
+        switch (c) {
+            case 0:
+                if (!strcmp(longopts[longopt_index].name, "help")) {
+                    do_help();
+                } else {
+                    assert(false);
+                }
+                break;  /* UNREACHABLE */
+            case '0': case '1': case '2': case '3': case '4':
+            case '5': case '6': case '7': case '8': case '9':
+                /* Notice that "diff -u4 -2" has the same behavior
+                 * as "diff -u42". This matches GNU. */
+                if (preceded_by_digit) {
+                    ocontext = 10*ocontext + (c - '0');
+                    ocontext = std::min<size_t>(ocontext, 1000);
+                } else {
+                    ocontext = (c - '0');
+                }
+                break;
+            case 'D':
+                g_PrintUsingIfdefs = true;
+                user_defined_macro_names.push_back(optarg);
+                break;
+            case 'U':
+            case 'u':
+                g_PrintUnifiedDiff = true;
+                if (optarg != NULL) {
+                    char *end;
+                    size_t value = strtoul(optarg, &end, 10);
+                    if (end == NULL || *end != '\0') {
+                        do_error("invalid context length '%s'", optarg);
+                    }
+                    g_LinesOfContext = std::max(g_LinesOfContext, value);
+                } else {
+                    g_LinesOfContext = std::max<size_t>(g_LinesOfContext, 3);
+                }
+                break;
         }
-        if (!strcmp(argv[i], "--")) { ++i; break; }
-        if (!strcmp(argv[i], "--help")) {
-            do_help();
-        } else if (argv[i][1] == 'D') {
-            const char *arg = (argv[i][2]=='\0' ? argv[++i] : argv[i]+2);
-            g_PrintUsingIfdefs = true;
-            if (arg == NULL) {
-                do_error("ERROR: option -D requires an argument");
-            }
-            user_defined_macro_names.push_back(arg);
-        } else if (!strcmp(argv[i], "--unified") || !strcmp(argv[i], "-u")) {
-            g_PrintUnifiedDiff = true;
-        } else if (argv[i][1] == 'U') {
-            const char *arg = (argv[i][2]=='\0' ? argv[++i] : argv[i]+2);
-            char *end;
-            g_PrintUnifiedDiff = true;
-            if (arg == NULL) {
-                do_error("ERROR: option -U requires an argument");
-            }
-            g_LinesOfContext = strtoul(arg, &end, 10);
-            if (end == arg || *end != '\0') {
-                do_error("ERROR: invalid context length '%s'", arg);
-            }
-        } else {
-            do_error("ERROR: unrecognized option %s", argv[i]);
-        }
+        preceded_by_digit = isdigit(c);
     }
-
-    const int num_files = argc - i;
+    
+    if (ocontext != (size_t)-1) {
+        g_LinesOfContext = ocontext;
+    }
+    
+    assert(optind <= argc);
+    const int num_files = argc - optind;
     const int num_user_defined_macros = user_defined_macro_names.size();
 
     if (g_PrintUnifiedDiff && g_PrintUsingIfdefs) {
-        do_error("ERROR: options --unified/-u/-U and --ifdef/-D are mutually exclusive");
+        do_error("options --unified/-u/-U and --ifdef/-D are mutually exclusive");
     }
 
     if (num_files == 0) {
-        do_error("ERROR: no files provided");
+        do_error("no files provided");
     }
     
     if (g_PrintUnifiedDiff && num_files != 2) {
-        do_error("ERROR: unified diff requires exactly two files");
+        do_error("unified diff requires exactly two files");
     }
     
-    if (num_user_defined_macros == 0) {
-        char buffer[8];
-        for (int j=0; j < num_files; ++j) {
-            sprintf(buffer, "V%d", j+1);
-            g_MacroNames.push_back(buffer);
+    if (g_PrintUsingIfdefs) {
+        assert(num_user_defined_macros >= 1);
+        if (num_user_defined_macros == num_files) {
+            g_MacroNames = user_defined_macro_names;
+        } else if (num_user_defined_macros > num_files) {
+            do_error("%d macro name(s) were provided via -D options, but only %d file(s)",
+                     num_user_defined_macros, num_files);
+        } else {
+            do_error("%d file(s) were provided, but only %d -D option(s)",
+                     num_files, num_user_defined_macros);
         }
-    } else if (num_user_defined_macros == num_files) {
-        g_MacroNames = user_defined_macro_names;
-    } else if (num_user_defined_macros > num_files) {
-        do_error("ERROR: %d macro name(s) were provided via -D options, but only %d file(s)",
-                 num_user_defined_macros, num_files);
-    } else {
-        do_error("ERROR: %d file(s) were provided, but only %d -D option(s)",
-                 num_files, num_user_defined_macros);
     }
 
     Difdef difdef(num_files);
-    for (int a = i; a < argc; ++a) {
-        std::ifstream f(argv[a]);
-        difdef.replace_file(a-i, f);
+    for (int i = 0; i < num_files; ++i) {
+        std::ifstream f(argv[optind + i]);
+        difdef.replace_file(i, f);
     }
 
     Difdef::Diff diff = difdef.merge();
 
     /* Print out the diff. */
     if (g_PrintUnifiedDiff) {
-        do_print_unified_diff(diff, argv+i);
+        do_print_unified_diff(diff, argv+optind);
     } else if (g_PrintUsingIfdefs) {
-        verify_properly_nested_directives(diff, argv+i);
+        verify_properly_nested_directives(diff, argv+optind);
         split_if_elif_ranges_by_version(diff);
         collapse_blank_lines(diff);
         do_print_using_ifdefs(diff);
